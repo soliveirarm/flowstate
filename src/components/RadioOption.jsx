72ddd866b6af5e213a85dd81bb60f9b2872d76ef
Sarah Oliveira <soliveirarm@gmail.com>
2026-02-23 06:50:51 -0300

    feat!: add lo-fi radio stations

--- src/components/RadioOption.jsx
+++ src/components/RadioOption.jsx
@@ -1,8 +1,8 @@
-export const RadioOption = ({ isSelected, radioName, onClick }) => (
+export const RadioOption = ({ isSelected, name, onClick }) => (
   <button
-    className={`radio-btn border-2 border-violet-300 font-semibold w-48 h-28 rounded-lg grow  ${isSelected ? "hover:opacity-90 border-none bg-violet-300 text-gray-950 inset-shadow-sm inset-shadow-violet-600/50" : "bg-gray-800 hover:bg-gray-700 text-violet-100"}`}
+    className={`radio-btn border-2 font-semibold rounded-full px-4 py-1 grow  ${isSelected ? "hover:opacity-90 border-gray-900 bg-violet-300 text-gray-950 inset-shadow-sm inset-shadow-violet-600/50" : "bg-gray-800 hover:bg-gray-700 border-gray-600 text-violet-100"}`}
     onClick={onClick}
   >
-    {radioName}
+    {name}
   </button>
 )
--- src/utils/notify.js
+++ src/utils/notify.js
@@ -1,5 +1,5 @@
-export const fireFocusNotification = () => {
-  if (Notification.permission === "granted") {
+export const notify = (userPreference) => {
+  if (Notification.permission === "granted" && userPreference) {
     new Notification("Break's over!", {
       body: "Time to get back to work.",
       icon: "./favicon.svg",
